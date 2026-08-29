40c74c0b08f6e842972058328b97a3ee72ec9f0a
7Prabhat <bloodandgravity@gmail.com>
2026-08-29 15:37:29 +0530

    2

--- part1.js
+++ part1.js
@@ -18,7 +18,9 @@ const expenses = [
   { id: 3, desc: "Wifi", amount: 840, paidBy: "rahul", participants: ["rahul", "aisha"] }
 ];
 
-let output = {};
+function totalPaid(expenses, members){
+  let output = {};
+
 
 members.forEach(member => {
 	output[member] = 0;
@@ -28,5 +30,43 @@ expenses.forEach(({paidBy, amount}) => {
 	output[paidBy] += amount;
 });
 
-console.log(output);
+  return output;
+};
+
+//splitEvenly(100, 3)
+//[33.34, 33.33, 33.33]
+//
+
+console.log("Q1 - Total paid by each member:");
+console.log(totalPaid(expenses, members));
+
+function splitEvenly(amount, parts){
+  //leftover paisa is given to first n people in array
+
+  let totalPaise = Math.round(amount * 100);
+  let basePaise = Math.floor(totalPaise / parts);
+  let remainder = totalPaise % parts;
+
+  let shares = [];
+
+  for(let i = 0; i < parts; i++)
+  {
+    let extra = (i < remainder) ? 1 : 0;
+
+    shares.push((basePaise + extra) / 100);
+  }
+  return shares;
+}
+
+console.log("Q2: Split 100 by 3:");
+let test1 = splitEvenly(100, 3);
+console.log("Output", test1);
+
+console.log("Proof sum:", test1.reduce((acc, val) => acc + val, 0).toFixed(2));
+
+console.log("Q2: Split 90 by 3:");
+let test2 = splitEvenly(90, 3);
+console.log("Ouput:", test2);
+console.log("Proof sum:", test2.reduce((acc, val) => acc + val, 0).toFixed(2));
+
 
